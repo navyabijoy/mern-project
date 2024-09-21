83cd51332362bbca2b9e3f082e6074996d04d631
Navya Bijoy <navyabijoy14@gmail.com>
2024-09-21 12:33:20 +0530

    created header

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Profile from "./pages/Profile";
 import Signin from "./pages/Signin";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Header from './components/header';
 
 export default function App() {
   return (
-    <BrowserRouter>  {/* Corrected BrowserRouter */}
+    <BrowserRouter>  
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
