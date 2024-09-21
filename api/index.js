226e9f7cef041ada243dbaf9d367ea49b9b8fda6
Navya Bijoy <navyabijoy14@gmail.com>
2024-09-21 16:17:05 +0530

    connected to the server

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
-
-const app = express()
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config()
+mongoose.connect('process.env.MONGO')
 
 app.listen(3000, () =>{
     console.log('Server is running on port 3000')
