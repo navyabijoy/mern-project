8c90234da25d183a094d9ba613bbbaff76f7d80a
Navya Bijoy <navyabijoy14@gmail.com>
2024-10-09 00:20:31 +0530

    eh

--- api/index.js
+++ api/index.js
@@ -3,9 +3,12 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() =>{
+mongoose
+.connect(process.env.MONGO)
+.then(() =>{
     console.log('connected to MongoDB');
-}).catch((err)=>{
+})
+.catch((err)=>{
     console.log(err);
 });
 
