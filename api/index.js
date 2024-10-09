0c87fe8ef97015270be2522e90f1daf28d60f205
Navya Bijoy <navyabijoy14@gmail.com>
2024-10-09 10:47:44 +0530

    created sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -19,5 +20,7 @@ const app = express();
 app.listen(3000, () =>{
     console.log('Server is running on port 3000')
 })
+app.use(express.json());
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
-app.use('/api/user', userRouter)
