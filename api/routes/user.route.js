746591f5aa4b3b69943478c635761ce8c7de4a23
Navya Bijoy <navyabijoy14@gmail.com>
2024-12-24 04:41:09 +0530

    done backend

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { test } from '../controllers/user.controller.js';
+import { deleteUser, test, updateUser,  getUserListings, getUser} from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
+
 
 const router = express.Router();
-//we need to 'GET' the information from the api
-router.get('/test', test);
 
-//all routes should be defined in the index.js
+router.get('/test', test);
+router.post('/update/:id', verifyToken, updateUser)
+router.delete('/delete/:id', verifyToken, deleteUser)
+router.get('/listings/:id', verifyToken, getUserListings)
+router.get('/:id', verifyToken, getUser)
 
 export default router;
