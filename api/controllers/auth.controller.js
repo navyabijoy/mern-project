51d235b165f51f4a7c12f8f8ac78f1c092d89513
Navya Bijoy <navyabijoy14@gmail.com>
2025-01-10 13:31:38 +0530

    add sign out user functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -69,3 +69,12 @@ catch(error){
   next(error)
   }
 }
+
+export const signOut = async(req,res,next) => {
+  try {
+    res.clearCookie('access_token')
+    res.status(200).json('User has been logged out!')
+  } catch(error){
+    next(error);
+  }
+}
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import { useRef, useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import supabase from "../../supabase/supabaseClient.js";
-import { deleteUserFailure, deleteUserStart, deleteUserSuccess, updateUserFailure, updateUserStart, updateUserSuccess } from "../redux/userSlice.js";
+import { deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserFailure, signOutUserStart, signOutUserSuccess, updateUserFailure, updateUserStart, updateUserSuccess } from "../redux/userSlice.js";
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -116,7 +116,22 @@ export default function Profile() {
       dispatch(deleteUserFailure(error.message))
     }
   }
-
+  
+  const handleSignOut = async() => {
+    try {
+      dispatch(signOutUserStart())
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json();
+      if (data.success === false){
+        dispatch(signOutUserFailure(data.message))
+        return
+      }
+      dispatch(signOutUserSuccess(data));
+  } catch(error) {
+    dispatch(signOutUserFailure(error.message))
+  }
+}
+  
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -174,7 +189,7 @@ export default function Profile() {
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete account</span>
-        <span className="text-red-700 cursor-pointer">Sign out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign out</span>
       </div>
 
       <p className="text-red-700 mt-5">{error ? error : ''}</p>
